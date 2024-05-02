a7b0b4fc1449da4b714b53bf058f541e3a1cf461
kupriianovvv <kupriianovvv@gmail.com>
2024-05-02 21:25:56 +0300

    tooltip storybook

---
+++ src/components/Tooltip/Tooltip.stories.tsx
@@ -0,0 +1,55 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Tooltip } from "./Tooltip";
+import { LegacyRef, forwardRef } from "react";
+
+const TestElement = forwardRef(
+  (
+    {
+      onMouseEnter,
+      onMouseLeave,
+    }: { onMouseEnter: () => void; onMouseLeave: () => void },
+    ref?: LegacyRef<HTMLDivElement>,
+  ) => {
+    return (
+      <div
+        ref={ref}
+        onMouseEnter={onMouseEnter}
+        onMouseLeave={onMouseLeave}
+        style={{
+          width: "200px",
+          height: "100px",
+          backgroundColor: "pink",
+        }}
+      >
+        testElement
+      </div>
+    );
+  },
+);
+
+const meta = {
+  title: "Components/Tooltip",
+  component: Tooltip,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: {
+    tooltipContent: "Tooltip text",
+    position: "top-start",
+    renderElementToWrap: (elementRef, onMouseEnter, onMouseLeave) => {
+      return (
+        <TestElement
+          ref={elementRef}
+          onMouseEnter={onMouseEnter}
+          onMouseLeave={onMouseLeave}
+        />
+      );
+    },
+  },
+} satisfies Meta<typeof Tooltip>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Primary: Story = {};
