c4cbe94f0d61a04efb74d84f0321746446c3a066
kupriianovvv <kupriianovvv@gmail.com>
2024-05-03 21:20:44 +0300

    portal

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,7 @@
 import { ModalOverlay } from "./components/ModalOverlay/ModalOverlay";
 import { ModalContent } from "./components/ModalContent/ModalContent";
 import { ReactNode } from "react";
+import { createPortal } from "react-dom";
 
 type ModalProps = {
   isOpened: boolean;
@@ -11,9 +12,9 @@ type ModalProps = {
 export const Modal = ({ isOpened, onClose, children }: ModalProps) => {
   if (!isOpened) return null;
   return (
-    <div>
+    createPortal(<div>
       <ModalOverlay onClose={onClose}/>
       <ModalContent>{children}</ModalContent>
-    </div>
+    </div>, document.getElementById('modal-root') as HTMLElement)
   );
 };
